f6cf60c1e95dbada1090cddd0f1df654dc05db28
Domenico Delvalle <domedfd@gmail.com>
2021-06-12 11:59:28 -0700

    criado componete Clock

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,19 +1,57 @@
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import React, { useEffect } from "react";
+import { View, StyleSheet, Animated, TouchableOpacity } from "react-native";
+import Constants from "expo-constants";
 
-export default () => (
-  <CountdownCircleTimer
-    isPlaying
-    duration={10}
-    colors={[
-      ['#004777', 0.4],
-      ['#F7B801', 0.4],
-      ['#A30000', 0.2],
-    ]}
-  >
-    {({ remainingTime, animatedColor }) => (
-      <Animated.Text style={{ color: animatedColor }}>
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+
+
+const Item = ({ remainingTime, animatedColor }) => {
+  useEffect(() => {
+    if (remainingTime === 1) {
+      console.log("teste");
+    }
+  }, [remainingTime]);
+
+  return (
+    <View>
+      <Animated.Text style={{ color: animatedColor, fontSize: 100 }}>
         {remainingTime}
       </Animated.Text>
-    )}
-  </CountdownCircleTimer>
-)
+    </View>
+  );
+};
+
+export default () => {
+  const [isPlaying, setIsPlaying] = React.useState(true);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setIsPlaying((prev) => !prev)}>
+        <CountdownCircleTimer
+          initialRemainingTime={13}
+          isPlaying={isPlaying}
+          duration={10}
+          colors={[
+            ["#004777", 0.4],
+            ["#F7B801", 0.4],
+            ["#A30000", 0.2],
+          ]}
+          onComplete={() => [true]}
+        >
+          {(props) => <Item {...props} />}
+        </CountdownCircleTimer>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+});
--- src/views/telaA.js
+++ src/views/telaA.js
@@ -1,69 +1,11 @@
 import * as React from "react";
-import {
-  View,
-  StyleSheet,
-  Animated,
-  Button,
-  Text,
-  TouchableOpacity,
-} from "react-native";
-import Constants from "expo-constants";
-import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
-import { useEffect } from "react";
-
-
-const Item = ({ remainingTime, animatedColor }) =>{
-
-
-useEffect(()=>{
-if (remainingTime === 1 ) {
- console.log("teste")
-}
-},[remainingTime])
-
-
-return(
-  <View>
-  <Animated.Text style={{ color: animatedColor, fontSize: 100 }}>
-    {remainingTime}
-  </Animated.Text>
-</View>
-)
-}
-
+import { View } from "react-native";
+import Clock from "../components/clock";
 
 export default () => {
-  const [isPlaying, setIsPlaying] = React.useState(true);
-  
-
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => setIsPlaying((prev) => !prev)}>
-        <CountdownCircleTimer
-          initialRemainingTime={13}
-          isPlaying={isPlaying}
-          duration={10}
-          colors={[
-            ["#004777", 0.4],
-            ["#F7B801", 0.4],
-            ["#A30000", 0.2],
-          ]}
-          onComplete={() => [true]}
-        >
-          {(props) => <Item {...props}/>}
-        </CountdownCircleTimer>
-      </TouchableOpacity>
+    <View>
+      <Clock />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: "#ecf0f1",
-    padding: 8,
-  },
-});

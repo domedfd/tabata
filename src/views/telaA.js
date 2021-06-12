cd8644e4a49c3145e1ec150903ac056174ed4c24
Domenico Delvalle <domedfd@gmail.com>
2021-06-12 08:02:15 -0700

    renderClock

--- src/views/telaA.js
+++ src/views/telaA.js
@@ -1,57 +1,69 @@
-import * as React from 'react';
-import {  View, StyleSheet, Animated, Button, Text, Touchable } from 'react-native';
-import Constants from 'expo-constants';
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import * as React from "react";
+import {
+  View,
+  StyleSheet,
+  Animated,
+  Button,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import Constants from "expo-constants";
+import { CountdownCircleTimer } from "react-native-countdown-circle-timer";
+import { useEffect } from "react";
 
 
-export default ()=> {
-  const [isPlaying, setIsPlaying] = React.useState(true)
+const Item = ({ remainingTime, animatedColor }) =>{
+
+
+useEffect(()=>{
+if (remainingTime === 1 ) {
+ console.log("teste")
+}
+},[remainingTime])
+
+
+return(
+  <View>
+  <Animated.Text style={{ color: animatedColor, fontSize: 100 }}>
+    {remainingTime}
+  </Animated.Text>
+</View>
+)
+}
+
+
+export default () => {
+  const [isPlaying, setIsPlaying] = React.useState(true);
   
+
   return (
     <View style={styles.container}>
-      <CountdownCircleTimer
-       initialRemainingTime={13}
-        isPlaying={isPlaying}
-        duration={10}
-        colors={[
-          ['#004777', 0.4],
-          ['#F7B801', 0.4],
-          ['#A30000', 0.2],
-        ]}
-        onComplete={() => [true]}
-    >
-      {({ remainingTime, animatedColor }) => (
-        <View>
-          <Text>aqui</Text>
-
-       <Animated.Text style={{ color: animatedColor, fontSize: 100 }}>
-          
-            {remainingTime}
-           
-        </Animated.Text>
-
-  
-         <Text>aqui</Text>
-    <Button title="Clique " onPress={() => setIsPlaying(prev => !prev)}/>
-          
-        </View>
-      )}
-      
-    </CountdownCircleTimer>
-    
-    
-  </View>
-  )
-}
+      <TouchableOpacity onPress={() => setIsPlaying((prev) => !prev)}>
+        <CountdownCircleTimer
+          initialRemainingTime={13}
+          isPlaying={isPlaying}
+          duration={10}
+          colors={[
+            ["#004777", 0.4],
+            ["#F7B801", 0.4],
+            ["#A30000", 0.2],
+          ]}
+          onComplete={() => [true]}
+        >
+          {(props) => <Item {...props}/>}
+        </CountdownCircleTimer>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
     padding: 8,
-  }
+  },
 });
-   
